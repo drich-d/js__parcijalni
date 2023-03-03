e889a8dd9d3b3876ac5f5ae0c36b2fb7f8cf86ed
drich_d <lmodric2311@gmail.com>
2023-03-03 20:37:21 +0100

    Sve radi sada

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { attendees } from "./data/data.js";
 
+let filteredResults = attendees;
+
 const tableBodyEl = document.querySelector("tbody");
 tableBodyEl.classList.add = ".table__body";
 
@@ -8,39 +10,36 @@ const InputFieldEl = document.querySelector(".input__field");
 InputFieldEl.addEventListener("keyup", (event) => {
   const searchTerm = event.target.value;
 
-  const filteredResults = attendees.filter((attendee) =>
+  filteredResults = attendees.filter((attendee) =>
     attendee.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
-  renderAttendees(
-    filteredResults,
-    "There are no attendees according to the entered term"
-  );
+  renderAttendees();
 });
 
-function renderAttendees(attendees) {
+function renderAttendees() {
   tableBodyEl.innerText = "";
   const table = document.querySelector(".table");
   const alertEl = document.querySelector(".alert");
 
-  if (attendees.length) {
+  if (filteredResults.length) {
     alertEl.style.display = "none";
 
     table.appendChild(tableBodyEl);
 
-    for (let i = 0; i < attendees.length; i++) {
+    for (let i = 0; i < filteredResults.length; i++) {
       const row = document.createElement("tr");
 
       const cell1 = document.createElement("td");
-      cell1.innerText = attendees[i].id;
+      cell1.innerText = filteredResults[i].id;
 
       const cell2 = document.createElement("td");
-      cell2.innerText = attendees[i].name;
+      cell2.innerText = filteredResults[i].name;
 
       const cell3 = document.createElement("td");
-      cell3.innerText = attendees[i].surname;
+      cell3.innerText = filteredResults[i].surname;
 
       const cell4 = document.createElement("td");
-      cell4.innerText = attendees[i].age;
+      cell4.innerText = filteredResults[i].age;
 
       row.appendChild(cell1);
       row.appendChild(cell2);
@@ -53,23 +52,24 @@ function renderAttendees(attendees) {
     alertEl.style.display = "block";
   }
 }
-renderAttendees(attendees);
+renderAttendees();
 
-function sortAttendees(attendees) {
+function sortAttendees() {
   const sort = document.querySelector(".select");
   sort.addEventListener("change", (event) => {
     const sortValue = event.target.value;
-    if (sortValue === "ascending") attendees.sort((a, b) => a.age - b.age);
+    if (sortValue === "ascending")
+      filteredResults.sort((a, b) => a.age - b.age);
     else if (sortValue === "descending") {
-      attendees.sort((a, b) => b.age - a.age);
+      filteredResults.sort((a, b) => b.age - a.age);
     } else {
-      attendees.sort((a, b) => a.id - b.id);
+      filteredResults.sort((a, b) => a.id - b.id);
     }
-    renderAttendees(attendees);
+    renderAttendees();
   });
 }
 
-sortAttendees(attendees);
+sortAttendees();
 
 // if (sortValue === "ascending") attendees.sort((a, b) => a.age - b.age);
 // else {
